74d17ca2fce7f281615fb16c4284de7c6cda2cc1
dalilagiu98 <148708941+dalilagiu98@users.noreply.github.com>
2024-04-12 17:05:43 +0200

    Exception Handler

--- src/main/java/dalilagiurgola9/U5W2D5/exceptions/ExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package dalilagiurgola9.U5W2D5.exceptions;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class ExceptionHandler {
-}
---
+++ src/main/java/dalilagiurgola9/U5W2D5/exceptions/ExceptionsHandler.java
@@ -0,0 +1,2 @@
+package dalilagiurgola9.U5W2D5.exceptions;public class ExceptionsHandler {
+}
